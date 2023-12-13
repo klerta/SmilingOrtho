6e1dbc4dedb47bfcd84ba5d448247c553712ec18
amineamdouni <amineamdouni24@gmail.com>
2023-12-13 12:40:27 +0100

    done

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -3,7 +3,7 @@ import "../i18nConfig";
 
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 export default function Root() {
   return (
     <>
